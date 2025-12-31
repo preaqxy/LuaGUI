c4c4270e5a08711a4319fa5739247407cdca9dc6
preaqxy <vortexprime36@gmail.com>
2025-12-31 18:16:50 +0100

    update

--- LUA GUI.lua
+++ LUA GUI.lua
@@ -4,6 +4,7 @@ local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
+OrionLib:CreateLabel("discord.gg/XEONHUB")
 local Mouse = LocalPlayer:GetMouse()
 local HttpService = game:GetService("HttpService")
 
